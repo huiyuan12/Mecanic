fbebe20635a0e7667e94e31aa2fe43d6cd2467ed
huiyuan12 <huiyuan_97@hotmail.com>
2017-02-21 13:55:29 +0100

    asd

---
+++ src/physics.cpp
@@ -0,0 +1,35 @@
+#include <imgui\imgui.h>
+#include <imgui\imgui_impl_glfw_gl3.h>
+
+bool show_test_window = true;
+void GUI() {
+	{	//FrameRate
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		//TODO
+	}
+
+	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+	if(show_test_window) {
+		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		ImGui::ShowTestWindow(&show_test_window);
+	}
+}
+
+void PhysicsInit() {
+	//TODO
+	//crear particulas
+
+}
+void PhysicsUpdate(float dt) {
+	//TODO
+	//euler velter
+	//actualizar
+	//calculos
+	//rebotes
+
+}
+void PhysicsCleanup() {
+	//TODO
+	//destruir
+}
